edf41e9fe453d76fca2f5cfeb19b094cf399d0d5
Marc <marcdwest@gmail.com>
2019-10-30 20:32:29 -0500

    add EditLogModal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
 import AddBtn from './components/layout/AddBtn';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -18,6 +19,7 @@ const App = () => {
 			<div className='container'>
 				<AddBtn />
 				<AddLogModal />
+				<EditLogModal />
 				<Logs />
 			</div>
 		</Fragment>

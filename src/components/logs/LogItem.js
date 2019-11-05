6e5903dbee24a2afc8c6c50de6830d65c1fe6d9c
Marc <marcdwest@gmail.com>
2019-11-04 19:34:33 -0600

    add setCurrent to onClick to edit logs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -4,11 +4,11 @@ import React from 'react';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { deleteLog } from '../../actions/logActions';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const LogItem = ({ log, deleteLog }) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
 	const onDelete = () => {
 		deleteLog(log.id);
 		M.toast({ html: 'Log Deleted' });
@@ -22,6 +22,7 @@ const LogItem = ({ log, deleteLog }) => {
 					className={`modal-trigger ${
 						log.attention ? 'red-text' : 'blue-text'
 					}`}
+					onClick={() => setCurrent(log)}
 				>
 					{log.message}
 				</a>
@@ -42,9 +43,10 @@ const LogItem = ({ log, deleteLog }) => {
 LogItem.propTypes = {
 	log: PropTypes.object.isRequired,
 	deleteLog: PropTypes.func.isRequired,
+	setCurrent: PropTypes.func.isRequired,
 };
 
 export default connect(
 	null,
-	{ deleteLog }
+	{ deleteLog, setCurrent }
 )(LogItem);

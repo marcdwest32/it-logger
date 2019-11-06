85802a6233f3308eb8c397fcf3dfa08e3f1f4aaf
Marc <marcdwest@gmail.com>
2019-11-05 18:39:05 -0600

    addTech adds to db

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
 	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
 
@@ -9,7 +12,12 @@ const AddTechModal = () => {
 		if (firstName === '' || lastName === '') {
 			M.toast({ html: 'Please enter a first and last name' });
 		} else {
-			console.log(`${lastName}, ${firstName}`);
+			addTech({
+				firstName,
+				lastName,
+			});
+			M.toast({ html: `${firstName} ${lastName} has been added as a tech` });
+			// clear fields
 			setFirstName('');
 			setLastName('');
 		}
@@ -59,4 +67,11 @@ const AddTechModal = () => {
 	);
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+	addTech: PropTypes.func.isRequired,
+};
+
+export default connect(
+	null,
+	{ addTech }
+)(AddTechModal);
